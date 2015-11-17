b09b67b755151a2a787d01cb67644dd6595445fb
Christopher Moore <chris@nuwavecommerce.com>
2015-11-16 19:59:06 -0700

    change paramater name

--- src/Folklore/GraphQL/GraphQLController.php
+++ src/Folklore/GraphQL/GraphQLController.php
@@ -4,18 +4,18 @@ use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
 class GraphQLController extends Controller {
-    
+
     public function query(Request $request)
     {
         $query = $request->get('query');
-        $params = $request->get('params');
-        
+        $params = $request->get('variables');
+
         if(is_string($params))
         {
             $params = json_decode($params, true);
         }
-        
+
         return app('graphql')->query($query, $params);
     }
-    
+
 }
