ebf48f703fea570669cc590f4a66909a0b8ee9d5
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-01-13 13:58:50 -0500

    Fix with global id decoding

--- src/Folklore/GraphQL/Relay/Relay.php
+++ src/Folklore/GraphQL/Relay/Relay.php
@@ -10,59 +10,59 @@ class Relay
 {
     protected $app;
     protected $graphql;
-    
+
     public function __construct($app)
     {
         $this->app = $app;
         $this->graphql = $app['graphql'];
     }
-    
+
     public function connectionField($config = [])
     {
         $field = new ConnectionField($config);
         return $field;
     }
-    
+
     public function connectionFieldFromEdgeType($edgeType, $config = [])
     {
         $typeName = array_get($edgeType->config, 'name');
         $connectionName = array_get($config, 'connectionTypeName', str_plural($typeName).'Connection');
-        
+
         $connectionType = new ConnectionType([
             'name' => $connectionName
         ]);
         $connectionType->setEdgeType($edgeType);
         GraphQL::addType($connectionType, $connectionName);
-        
+
         $fieldConfig = array_except($config, ['connectionTypeName']);
         $field = new ConnectionField($fieldConfig);
         $field->setType(GraphQL::type($connectionName));
         return $field;
     }
-    
+
     public function connectionFieldFromEdgeTypeAndQueryBuilder($edgeType, $queryBuilderResolver, $config = [])
     {
         $field = $this->connectionFieldFromEdgeType($edgeType, $config);
         $field->setQueryBuilderResolver($queryBuilderResolver);
         return $field;
     }
-    
+
     public function toGlobalId($type, $id)
     {
         return base64_encode($type.':'.$id);
     }
-    
+
     public function fromGlobalId($globalId)
     {
-        return explode(':', base64_decode($globalId));
+        return explode(':', base64_decode($globalId), 2);
     }
-    
+
     public function getIdFromGlobalId($globalId)
     {
         list($type, $id) = $this->fromGlobalId($globalId);
         return $id;
     }
-    
+
     public function getTypeFromGlobalId($globalId)
     {
         list($type, $id) = $this->fromGlobalId($globalId);
