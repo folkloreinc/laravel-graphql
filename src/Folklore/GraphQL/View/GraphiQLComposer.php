34eecb7a0122ca6f12213a0d1becb275d6033106
Kazuhisa Togo <kazuhisa@ktogo.net>
2017-06-06 16:17:10 +0900

    Add custom schema support on GraphiQL

--- src/Folklore/GraphQL/View/GraphiQLComposer.php
+++ src/Folklore/GraphQL/View/GraphiQLComposer.php
@@ -1,11 +1,16 @@
 <?php
-
 namespace Folklore\GraphQL\View;
+
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Route;
 
 class GraphiQLComposer
 {
-    public function compose($view)
+    public function compose(View $view)
     {
         $view->graphqlPath = app('router')->has('graphql.query') ? route('graphql.query') : url('/graphql');
+        if ('' !== $schema = (string) Route::current()->parameter('graphql_schema')) {
+            $view->graphqlPath .= '/'. $schema;
+        }
     }
 }
