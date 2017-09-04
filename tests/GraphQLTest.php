26d6fafca8442f59dde28b7d608f53a150ff8a46
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-09-04 15:05:31 -0400

    Fix with error

--- tests/GraphQLTest.php
+++ tests/GraphQLTest.php
@@ -285,7 +285,7 @@ class GraphQLTest extends TestCase
         ]);
         $validator->fails();
         $validationError = with(new ValidationError('validation'))->setValidator($validator);
-        $error = new Error('error', null, $validationError);
+        $error = new Error('error', null, null, null, null, $validationError);
         $error = $this->graphql->formatError($error);
 
         $this->assertInternalType('array', $error);
