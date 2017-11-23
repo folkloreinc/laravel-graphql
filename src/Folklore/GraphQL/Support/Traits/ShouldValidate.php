04d85b831b7944bbac3ae8b74f2e2dd80f7e30c1
Stefan Horner <shorner@golin.com>
2017-11-23 12:46:12 +0000

    scan input object types for validation rules

--- src/Folklore/GraphQL/Support/Traits/ShouldValidate.php
+++ src/Folklore/GraphQL/Support/Traits/ShouldValidate.php
@@ -3,6 +3,9 @@
 namespace Folklore\GraphQL\Support\Traits;
 
 use Folklore\GraphQL\Error\ValidationError;
+use GraphQL\Type\Definition\InputObjectType;
+use GraphQL\Type\Definition\ListOfType;
+use GraphQL\Type\Definition\WrappingType;
 
 trait ShouldValidate
 {
@@ -25,9 +28,58 @@ trait ShouldValidate
                     $argsRules[$name] = $arg['rules'];
                 }
             }
+
+            if (isset($arg['type'])) {
+                $argsRules = array_merge($argsRules, $this->inferRulesFromType($arg['type'], $name));
+            }
         }
 
         return array_merge($rules, $argsRules);
+    }
+
+    public function inferRulesFromType($type, $prefix)
+    {
+        $rules = [];
+
+        // if it is an array type, add an array validation component
+        if ($type instanceof ListOfType) {
+            $prefix = "{$prefix}.*";
+        }
+
+        // make sure we are dealing with the actual type
+        if ($type instanceof WrappingType) {
+            $type = $type->getWrappedType();
+        }
+
+        // if it is an input object type - the only type we care about here...
+        if ($type instanceof InputObjectType) {
+            // merge in the input type's rules
+            $rules = array_merge($rules, $this->getInputTypeRules($type, $prefix));
+        }
+
+        // Ignore scalar types
+
+        return $rules;
+    }
+
+    public function getInputTypeRules(InputObjectType $input, $prefix)
+    {
+        $rules = [];
+
+        foreach ($input->getFields() as $name => $field) {
+            $key = "{$prefix}.{$name}";
+
+            // get any explicitly set rules
+            if (isset($field->rules)) {
+                $rules[$key] = $field->rules;
+            }
+
+            // then recursively call the parent method to see if this is an
+            // input object, passing in the new prefix
+            array_merge($rules, $this->inferRulesFromType($field->type, $key));
+        }
+
+        return $rules;
     }
 
     protected function getValidator($args, $rules)
