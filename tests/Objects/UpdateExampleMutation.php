c3b4a27a0ebf1abf6e8ff80e07b12c4d3e51d743
Stefan Horner <shorner@golin.com>
2017-11-23 14:25:56 +0000

    test nested and list validation

--- tests/Objects/UpdateExampleMutation.php
+++ tests/Objects/UpdateExampleMutation.php
@@ -1,20 +1,21 @@
 <?php
 
-use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Facades\GraphQL;
 use Folklore\GraphQL\Support\Mutation;
+use GraphQL\Type\Definition\Type;
 
 class UpdateExampleMutation extends Mutation
 {
-    
+
     protected $attributes = [
         'name' => 'updateExample'
     ];
-    
+
     public function type()
     {
         return GraphQL::type('Example');
     }
-    
+
     public function rules()
     {
         return [
@@ -29,20 +30,26 @@ class UpdateExampleMutation extends Mutation
                 'name' => 'test',
                 'type' => Type::string()
             ],
-            
+
             'test_with_rules' => [
                 'name' => 'test',
                 'type' => Type::string(),
                 'rules' => ['required']
             ],
-            
+
             'test_with_rules_closure' => [
                 'name' => 'test',
                 'type' => Type::string(),
                 'rules' => function () {
                     return ['required'];
                 }
-            ]
+            ],
+
+            'test_with_rules_input_object' => [
+                'name' => 'test',
+                'type' => GraphQL::type('ExampleValidationInputObject'),
+                'rules' => ['required'],
+            ],
         ];
     }
 
