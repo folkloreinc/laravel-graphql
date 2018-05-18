467f790d6a5b58d792590119c074e2ed575e0473
spawnia <benedikt@franke.tech>
2018-05-18 10:04:57 +0200

    Add type hints to ValidationError

--- src/Folklore/GraphQL/Error/ValidationError.php
+++ src/Folklore/GraphQL/Error/ValidationError.php
@@ -1,26 +1,39 @@
 <?php namespace Folklore\GraphQL\Error;
 
 use GraphQL\Error\Error;
-use GraphQL\FormattedError;
+use Illuminate\Validation\Validator;
 
 class ValidationError extends Error
 {
+    /**
+     * @var Validator
+     */
     public $validator;
-    
-    public function setValidator($validator)
+
+    /**
+     * @param Validator $validator
+     * @return $this
+     */
+    public function setValidator(Validator $validator)
     {
         $this->validator = $validator;
         
         return $this;
     }
-    
+
+    /**
+     * @return Validator
+     */
     public function getValidator()
     {
         return $this->validator;
     }
-    
+
+    /**
+     * @return array|\Illuminate\Support\MessageBag
+     */
     public function getValidatorMessages()
     {
-        return $this->validator ? $this->validator->messages():[];
+        return $this->validator ? $this->validator->messages() : [];
     }
 }
