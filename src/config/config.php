ebba2beb03b5cfdecf7914bf91fce7ac329d7452
Johannes Nagl <johannes@nagl.name>
2018-06-13 14:17:33 +0200

    fix link to security page

--- src/config/config.php
+++ src/config/config.php
@@ -199,7 +199,7 @@ return [
 
     /*
      * Options to limit the query complexity and depth. See the doc
-     * @ https://github.com/webonyx/graphql-php#security
+     * @ https://github.com/webonyx/graphql-php/blob/master/docs/security.md#disabling-introspection
      * for details. Disabled by default.
      */
     'security' => [
